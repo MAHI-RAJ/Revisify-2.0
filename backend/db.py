0ef157684c6825503e8ac50f7da5e7d367167763
SahilAliAkbar <114466921+SahilAliAkbar@users.noreply.github.com>
2025-12-08 15:12:42 +0530

    Add files via upload

--- backend/db.py
+++ backend/db.py
@@ -1,13 +1,8 @@
 """Database initialization"""
-
 from extensions import db
 
 def init_db(app):
-    """Initialize database and create tables."""
-    # If you already call db.init_app(app) in app.py, you can remove the next line.
-    db.init_app(app)
-
+    """Initialize database"""
     with app.app_context():
-        # IMPORTANT: ensure models are imported so tables are registered
-        import models  # noqa: F401
         db.create_all()
+
